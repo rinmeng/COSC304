e8047d30fead32d5fa94625e45ce92c7b4bc03ba
rin! <phurinmeng@gmail.com>
2024-10-29 15:14:01 -0700

    docker run

--- 304_lab6/assignPython/code/EnrollDB.py
+++ 304_lab6/assignPython/code/EnrollDB.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 class EnrollDB:
     """Application for an enrollment database on MySQL"""
 
@@ -8,81 +9,86 @@ class EnrollDB:
         try:
             # TODO: Fill in your connection information
             print("Connecting to database.")
-            self.cnx = mysql.connector.connect(user='testuser', password='304testpw', host='localhost', database='testuser')
+            self.cnx = mysql.connector.connect(
+                user="testuser",
+                password="304testpw",
+                host="localhost",
+                database="testuser",
+            )
             return self.cnx
-        except mysql.connector.Error as err:  
-            print(err)   
+        except mysql.connector.Error as err:
+            print(err)
 
     def init(self):
         """Creates and initializes the database"""
         fileName = "university.ddl"
         print("Loading data")
-        
+
         try:
             cursor = self.cnx.cursor()
             with open(fileName, "r") as infile:
                 st = infile.read()
                 commands = st.split(";")
-                for line in commands:                   
+                for line in commands:
                     # print(line.strip("\n"))
                     line = line.strip()
                     if line == "":  # Skip blank lines
-                        continue 
-                        
-                    cursor.execute(line)                    
-                        
+                        continue
+
+                    cursor.execute(line)
+
             cursor.close()
-            self.cnx.commit()            
+            self.cnx.commit()
             print("Database load complete")
-        except mysql.connector.Error as err:  
+        except mysql.connector.Error as err:
             print(err)
-            self.cnx.rollback()  
-        
+            self.cnx.rollback()
+
     def close(self):
         try:
             print("Closing database connection.")
             self.cnx.close()
-        except mysql.connector.Error as err:  
-            print(err)   
+        except mysql.connector.Error as err:
+            print(err)
 
     def listAllStudents(self):
-        """ Returns a String with all the students in the database.  
-            Format:
-                sid, sname, sex, birthdate, gpa
-                00005465, Joe Smith, M, 1997-05-01, 3.20
+        """Returns a String with all the students in the database.
+        Format:
+            sid, sname, sex, birthdate, gpa
+            00005465, Joe Smith, M, 1997-05-01, 3.20
 
-            Return:
-                String containing all student information"""
+        Return:
+            String containing all student information"""
 
         print("Executing list all students.")
-        
+
         output = "sid, sname, sex, birthdate, gpa"
         cursor = self.cnx.cursor()
-        # TODO: Execute query and build output string                
+        # TODO: Execute query and build output string
         cursor.close()
-        
+
         return output
 
     def listDeptProfessors(self, deptName):
         """Returns a String with all the professors in a given department name.
-           Format:
-                    Professor Name, Department Name
-                    Art Funk, Computer Science
-           Returns:
-                    String containing professor information"""
+        Format:
+                 Professor Name, Department Name
+                 Art Funk, Computer Science
+        Returns:
+                 String containing professor information"""
 
-        # TODO: Execute query and build output string 
+        # TODO: Execute query and build output string
         return ""
 
     def listCourseStudents(self, courseNum):
         """Returns a String with all students in a given course number (all sections).
-            Format:
-                Student Id, Student Name, Course Number, Section Number
-                00005465, Joe Smith, COSC 304, 001
-            Return:
-                 String containing students"""
+        Format:
+            Student Id, Student Name, Course Number, Section Number
+            00005465, Joe Smith, COSC 304, 001
+        Return:
+             String containing students"""
 
-        # TODO: Execute query and build output string 
+        # TODO: Execute query and build output string
         return ""
 
     def computeGPA(self, studentId):
@@ -95,8 +101,8 @@ class EnrollDB:
         """Inserts a student into the databases."""
 
         # TODO: Execute statement. Make sure to commit
-        return  
-    
+        return
+
     def deleteStudent(self, studentId):
         """Deletes a student from the databases."""
 
@@ -108,15 +114,15 @@ class EnrollDB:
 
         # TODO: Execute statement. Make sure to commit
         return
-        
+
     def newEnroll(self, studentId, courseNum, sectionNum, grade):
         """Creates a new enrollment in a course section."""
-        
+
         # TODO: Execute statement. Make sure to commit
         return
 
     def updateStudentGPA(self, studentId):
-        """ Updates a student's GPA based on courses taken."""
+        """Updates a student's GPA based on courses taken."""
 
         # TODO: Execute statement. Make sure to commit
         return
@@ -140,48 +146,48 @@ class EnrollDB:
         return None
 
     def query2(self):
-        """For each student return their id and name, number of course sections registered in (called numcourses), and gpa (average of grades). 
+        """For each student return their id and name, number of course sections registered in (called numcourses), and gpa (average of grades).
         Return only students born after March 15, 1992. A student is also only in the result if their gpa is above 3.1 or registered in 0 courses.
         Order by GPA descending then student name ascending and show only the top 5."""
-
-         # TODO: Execute the query and return a cursor
-        return None
-
-    def query3(self):
-        """For each course, return the number of sections (numsections), total number of students enrolled (numstudents), average grade (avggrade), and number of distinct professors who taught the course (numprofs).
-            Only show courses in Chemistry or Computer Science department. Make sure to show courses even if they have no students. Do not show a course if there are no professors teaching that course. 
-            Format:
-            cnum, numsections, numstudents, avggrade, numprof"""
-
-         # TODO: Execute the query and return a cursor
-        return None
-
-    def query4(self):
-        """Return the students who received a higher grade than their course section average in at least two courses. Order by number of courses higher than the average and only show top 5.
-            Format:
-            EmployeeId, EmployeeName, orderCount"""
 
         # TODO: Execute the query and return a cursor
         return None
 
+    def query3(self):
+        """For each course, return the number of sections (numsections), total number of students enrolled (numstudents), average grade (avggrade), and number of distinct professors who taught the course (numprofs).
+        Only show courses in Chemistry or Computer Science department. Make sure to show courses even if they have no students. Do not show a course if there are no professors teaching that course.
+        Format:
+        cnum, numsections, numstudents, avggrade, numprof"""
 
-# Do NOT change anything below here
+        # TODO: Execute the query and return a cursor
+        return None
+
+    def query4(self):
+        """Return the students who received a higher grade than their course section average in at least two courses. Order by number of courses higher than the average and only show top 5.
+        Format:
+        EmployeeId, EmployeeName, orderCount"""
+
+        # TODO: Execute the query and return a cursor
+        return None
+
+    # Do NOT change anything below here
     def resultSetToString(self, cursor, maxrows):
         output = ""
         if cursor is None:
             return "No results"
         cols = cursor.column_names
-        output += "Total columns: "+str(len(cols))+"\n"
+        output += "Total columns: " + str(len(cols)) + "\n"
         output += str(cols[0])
-        for i in range(1,len(cols)):
-            output += ", "+str(cols[i])
+        for i in range(1, len(cols)):
+            output += ", " + str(cols[i])
         for row in cursor:
-            output += "\n"+str(row[0])
-            for i in range(1,len(cols)):
-                output += ", "+str(row[i])
-        output += "\nTotal results: "+str(cursor.rowcount)
+            output += "\n" + str(row[0])
+            for i in range(1, len(cols)):
+                output += ", " + str(row[i])
+        output += "\nTotal results: " + str(cursor.rowcount)
         return output
-                
+
+
 # Main execution for testing
 enrollDB = EnrollDB()
 enrollDB.connect()
@@ -200,17 +206,17 @@ print("Executing list students in course: DATA 301")
 enrollDB.listCourseStudents("DATA 301")
 
 print("Executing compute GPA for student: 45671234")
-enrollDB.resultSetToString(enrollDB.computeGPA("45671234"),10)
+enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10)
 print("Executing compute GPA for student: 00000000")
-enrollDB.resultSetToString(enrollDB.computeGPA("45671234"),10)
+enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10)
 
 print("Adding student 55555555:")
-enrollDB.addStudent("55555555",  "Stacy Smith", "F", "1998-01-01")
+enrollDB.addStudent("55555555", "Stacy Smith", "F", "1998-01-01")
 print("Adding student 11223344:")
-enrollDB.addStudent("11223344",  "Jim Jones", "M",  "1997-12-31")
+enrollDB.addStudent("11223344", "Jim Jones", "M", "1997-12-31")
 print(enrollDB.listAllStudents())
 
-print("Test delete student:");
+print("Test delete student:")
 print("Deleting student 99999999:")
 enrollDB.deleteStudent("99999999")
 # Non-existing student
@@ -219,9 +225,9 @@ enrollDB.deleteStudent("00000000")
 print(enrollDB.listAllStudents())
 
 print("Updating student 99999999:")
-enrollDB.updateStudent("99999999",  "Wang Wong", "F", "1995-11-08", 3.23)
+enrollDB.updateStudent("99999999", "Wang Wong", "F", "1995-11-08", 3.23)
 print("Updating student 00567454:")
-enrollDB.updateStudent("00567454",  "Scott Brown", "M",  None, 4.00)
+enrollDB.updateStudent("00567454", "Scott Brown", "M", None, 4.00)
 print(enrollDB.listAllStudents())
 
 print("Test new enrollment in COSC 304 for 98123434:")
@@ -229,7 +235,7 @@ enrollDB.newEnroll("98123434", "COSC 304", "001", 2.51)
 
 enrollDB.init()
 print("Test update student GPA for student:")
-enrollDB.newEnroll("98123434", "COSC 304", "001", 3.97);  
+enrollDB.newEnroll("98123434", "COSC 304", "001", 3.97)
 enrollDB.updateStudentGPA("98123434")
 
 print("Test update student mark for student 98123434 to 3.55:")
@@ -246,6 +252,5 @@ print(enrollDB.resultSetToString(enrollDB.query1(), 100))
 print(enrollDB.resultSetToString(enrollDB.query2(), 100))
 print(enrollDB.resultSetToString(enrollDB.query3(), 100))
 print(enrollDB.resultSetToString(enrollDB.query4(), 100))
-        
-enrollDB.close()
 
+enrollDB.close()
